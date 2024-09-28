4b24ab81f42805170ffb3d92baf7634f03f08ed1
Peng Wang <pw7nc@virginia.edu>
2024-09-28 19:25:21 -0400

    merge the training scripts

--- run_reward_modeling.sh
+++ run_reward_modeling.sh
@@ -1,11 +1,16 @@
 # Set which GPU devices to be visible to the process, --num_processes should be adjusted accordingly
-export CUDA_VISIBLE_DEVICES="2,3"
+export CUDA_VISIBLE_DEVICES="3"
+export WANDB_PROJECT="ensemble reward model with LoRA"
 
 port=$(shuf -i 6000-9000 -n 1)
 echo $port
 
 # Set the logging level for the `accelerate` library to output informational messages.
 ACCELERATE_LOG_LEVEL=info
+
+######## Reward Model base models ########
+# model_name='EleutherAI/gpt-j-6b'
+model_name='meta-llama/Meta-Llama-3-8B'
 
 # accelerate launch --config_file configs/deepspeed_zero2.yaml --num_processes=1 --main_process_port=${port} src/reward_modeling.py \
 #     --model_name_or_path EleutherAI/gpt-j-6b \
@@ -31,16 +36,16 @@ ACCELERATE_LOG_LEVEL=info
 #     --report_to wandb \
 
 # Train the model
-accelerate launch --config_file configs/deepspeed_zero2.yaml --num_processes=2 --main_process_port=${port} src/reward_modeling.py \
+accelerate launch --config_file configs/deepspeed_zero2.yaml --num_processes=4 --main_process_port=${port} src/reward_modeling.py \
     --model_name_or_path EleutherAI/gpt-j-6b \
     --dataset_name openai/summarize_from_feedback \
     --dataset_subset comparisons \
     --selected_labeler personalized \
-    --output_dir ./exp/gpt-j-6b-Reward/lr-5e-5-3epochs-lorar32/pslora-diag \
-    --per_device_train_batch_size 64 \
-    --per_device_eval_batch_size 128 \
+    --output_dir ./exp/gpt-j-6b-Reward/lr-5e-5-3epochs-lorar32/pslora-kernel-multigpu \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 16 \
     --num_train_epochs 3 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 8 \
     --remove_unused_columns False \
     --gradient_checkpointing True \
     --bf16 true \
@@ -54,6 +59,6 @@ accelerate launch --config_file configs/deepspeed_zero2.yaml --num_processes=2 -
     --lora_alpha 16 \
     --lora_task_type SEQ_CLS \
     --lora_target_modules q_proj v_proj \
-    --lora_type svd \
+    --lora_type kernel \
     --save_only_model True \
     --report_to wandb \
